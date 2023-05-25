46340dd9d7383bf806e69f324e18d7bd31335935
Red Hat <sonjuhy@gmail.com>
2023-05-25 22:20:17 +0900

    ✨ ADD axios feature
    - ADD axios feature

--- components/card/NoticeCard.tsx
+++ components/card/NoticeCard.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 function WithHeaderAndQuoteExample() {
@@ -6,6 +7,19 @@ function WithHeaderAndQuoteExample() {
     const [content, setContent] = useState('This is a space that displays what you have written as a notice.');
     const [writer, setWriter] = useState('writer');
     
+    async function getLastNotice(){
+      const data:any = await axios.request({
+        url : process.env.BASE_URL+'/notice/getTopNotice',
+        method: 'GET',
+      })
+      var notice = data.data;
+      setTitle(notice.title);
+      setContent(notice.content);
+      setWriter(notice.writer);
+    }
+  useEffect(()=>{
+    getLastNotice();
+  })
   return (
     <Card className='shadow p-3 mb-5 rounded'>
       {/* <Card.Header>{title}</Card.Header> */}
