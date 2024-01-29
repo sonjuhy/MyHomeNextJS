4289d737ceb027a57a5556e7cb12001410552619
Red Hat <sonjuhy@gmail.com>
2024-01-30 00:01:20 +0900

    ✨ Change redux-toolkit & redux-persist to new version
    - Change redux-toolkit & redux-persist to new version

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import { StoreProvider } from '../StoreProvider'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <StoreProvider>
           <Main />
           <NextScript />
-        </StoreProvider>
       </body>
     </Html>
   )
